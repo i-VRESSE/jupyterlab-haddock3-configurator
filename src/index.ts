0e614de58032c0cd67dc1b4b800777873d44b3c6
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-08-08 13:04:03 +0200

    Make activate()

--- src/index.ts
+++ src/index.ts
@@ -7,60 +7,64 @@ import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
 import { Haddock3ConfiguratorWidget } from './widget';
 
+const pluginId = 'haddock3-configurator:plugin'
+
+const activate = (
+  app: JupyterFrontEnd,
+  palette: ICommandPalette,
+  settingRegistry: ISettingRegistry | null
+): void => {
+  console.log('JupyterLab extension haddock3-configurator is activated!');
+  console.log('ICommandPalette:', palette);
+
+  if (settingRegistry) {
+    settingRegistry
+      .load(pluginId)
+      .then(settings => {
+        console.log(
+          'haddock3-configurator settings loaded:',
+          settings.composite
+        );
+      })
+      .catch(reason => {
+        console.error(
+          'Failed to load settings for haddock3-configurator.',
+          reason
+        );
+      });
+  }
+
+  const content = new Haddock3ConfiguratorWidget();
+  const widget = new MainAreaWidget({ content });
+  widget.id = 'h3c-jupyterlab';
+  widget.title.label = 'Haddock3 config';
+  widget.title.closable = true;
+
+  const command = 'h3c:create-new';
+  app.commands.addCommand(command, {
+    label: 'Create haddock3 config',
+    execute: () => {
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.add(widget, 'main');
+      }
+      // Activate the widget
+      app.shell.activateById(widget.id);
+    }
+  });
+
+  palette.addItem({ command, category: 'Haddock3' });
+};
+
 /**
  * Initialization data for the haddock3-configurator extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'haddock3-configurator:plugin',
+  id: pluginId,
   autoStart: true,
   optional: [ISettingRegistry],
   requires: [ICommandPalette],
-  activate: (
-    app: JupyterFrontEnd,
-    palette: ICommandPalette,
-    settingRegistry: ISettingRegistry | null
-  ) => {
-    console.log('JupyterLab extension haddock3-configurator is activated!');
-    console.log('ICommandPalette:', palette);
-
-    if (settingRegistry) {
-      settingRegistry
-        .load(plugin.id)
-        .then(settings => {
-          console.log(
-            'haddock3-configurator settings loaded:',
-            settings.composite
-          );
-        })
-        .catch(reason => {
-          console.error(
-            'Failed to load settings for haddock3-configurator.',
-            reason
-          );
-        });
-    }
-
-    const content = new Haddock3ConfiguratorWidget();
-    const widget = new MainAreaWidget({ content });
-    widget.id = 'h3c-jupyterlab';
-    widget.title.label = 'Haddock3 config';
-    widget.title.closable = true;
-
-    const command = 'h3c:create-new';
-    app.commands.addCommand(command, {
-      label: 'Create haddock3 config',
-      execute: () => {
-        if (!widget.isAttached) {
-          // Attach the widget to the main work area if it's not there
-          app.shell.add(widget, 'main');
-        }
-        // Activate the widget
-        app.shell.activateById(widget.id);
-      }
-    });
-
-    palette.addItem({ command, category: 'Haddock3' });
-  }
+  activate
 };
 
 export default plugin;
