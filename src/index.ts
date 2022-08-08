de3567e07aca835d2939c0765fc176a7334b237c
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-08-08 11:35:25 +0200

    Show an empty panel
    https://jupyterlab.readthedocs.io/en/stable/extension/extension_tutorial.html#show-an-empty-panel

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import {
 } from '@jupyterlab/application';
 
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { Widget } from '@lumino/widgets';
 
 /**
  * Initialization data for the haddock3-configurator extension.
@@ -12,19 +14,52 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'haddock3-configurator:plugin',
   autoStart: true,
   optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+  requires: [ICommandPalette],
+  activate: (
+    app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    settingRegistry: ISettingRegistry | null
+  ) => {
     console.log('JupyterLab extension haddock3-configurator is activated!');
+    console.log('ICommandPalette:', palette);
 
     if (settingRegistry) {
       settingRegistry
         .load(plugin.id)
         .then(settings => {
-          console.log('haddock3-configurator settings loaded:', settings.composite);
+          console.log(
+            'haddock3-configurator settings loaded:',
+            settings.composite
+          );
         })
         .catch(reason => {
-          console.error('Failed to load settings for haddock3-configurator.', reason);
+          console.error(
+            'Failed to load settings for haddock3-configurator.',
+            reason
+          );
         });
     }
+
+    const content = new Widget();
+    const widget = new MainAreaWidget({ content });
+    widget.id = 'h3c-jupyterlab';
+    widget.title.label = 'Haddock3 config';
+    widget.title.closable = true;
+
+    const command = 'h3c:create-new';
+    app.commands.addCommand(command, {
+      label: 'Create haddock3 config',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    palette.addItem({ command, category: 'Haddock3' });
   }
 };
 
